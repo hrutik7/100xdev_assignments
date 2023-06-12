19f3c2d145a61b8e4e9b19fc5ee6b82e599ac615
hrutik7 <62372737+hrutik7@users.noreply.github.com>
2023-06-12 12:52:41 +0530

    anagram

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -8,7 +8,28 @@
 */
 
 function isAnagram(str1, str2) {
+    var str1 = str1.split('')
+    var str2 = str2.split('')
+    var count = 0
+    for(var i = 0 ; i<str1.length;i++){
+      for(var j = 0 ;j<str2.length;j++){
+        if(str1[i] === str2[j]){
+          console.log(count,str2.length,"count")
+          count++
+        }
+      }
+    }
 
+    if(count === str2.length && str2.length === str1.length){
+      return true
+    }
+}
+
+var ans = isAnagram('cat', 'act');
+if(ans){
+  console.log("this is an anagram")
+}else{
+  console.log("not an anagram")
 }
 
 module.exports = isAnagram;
